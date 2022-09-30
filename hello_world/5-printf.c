61b2a162fd986d64a8ea55b2a5b60bfa687aa6b7
Katrolli <ennio_cano@live.com>
2022-09-30 02:43:19 -0700

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char MyString[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", MyString);
+	return (0);
+}
