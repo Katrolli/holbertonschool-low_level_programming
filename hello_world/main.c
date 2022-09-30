6594ca1d5e4283890fc897bcdc360b2af775d623
Katrolli <ennio_cano@live.com>
2022-09-30 02:31:42 -0700

    first string

--- hello_world/main.c
+++ hello_world/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * main - Entry point
@@ -7,5 +8,8 @@
  */
 int main(void)
 {
-return (0);
+	char MyString[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(MyString);
+	return (0);
 }
