ed9196c06f0abaebeeaaa7d61eff5d67fcf35047
Katrolli <ennio_cano@live.com>
2022-09-30 03:24:57 -0700

    value

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	double doubleType;
 	/* sizeof returns size of variable */
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes(s)\n", sizeof(intType));
-	printf("Size of a long long int: %zu bytes(s)\n", sizeof(doubleType));
-	printf("Size of a float: %zu bytes(s)\n", sizeof(floatType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doubleType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
