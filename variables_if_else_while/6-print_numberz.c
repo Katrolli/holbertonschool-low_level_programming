3713cfb1956becf34a32543d9469ccb3067854d2
Katrolli <ennio_cano@live.com>
2022-10-03 08:02:30 -0700

    1 to 9 with no char

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this is the entry point of the program
+ * Return: returns 0 if sucess
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
