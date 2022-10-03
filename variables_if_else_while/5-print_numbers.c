fc2ec5b15fedee3c9b4c5c923d5764f124e6660c
Katrolli <ennio_cano@live.com>
2022-10-03 07:58:07 -0700

    1 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this is the entry point of the program
+ * Return: returns 0 if sucess
+ */
+int main(void)
+{
+	char ch;
+
+
+	char n = '\n';
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(n);
+
+	return (0);
+}
