b79298670ee073caa66881d41e8ce864abba420b
Katrolli <ennio_cano@live.com>
2022-10-03 06:20:14 -0700

    print lowercase alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this is the entry point of the program
+ * Return: returns 0 if sucess
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
