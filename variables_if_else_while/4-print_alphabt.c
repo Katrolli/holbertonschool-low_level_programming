1b5cbdfaf425049e878d7e577441a26e27fc83ff
Katrolli <ennio_cano@live.com>
2022-10-03 07:48:04 -0700

    exept q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - this is the entry point of the program
+ * Return: returns 0 if sucess
+ */
+int main(void)
+{
+	char ch;
+
+
+	char n = '\n';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+
+	}
+	putchar(n);
+
+	return (0);
+}
