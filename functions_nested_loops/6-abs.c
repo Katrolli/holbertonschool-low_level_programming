77548d22cb5b8faf2a8353c2e61b204f22c1ec53
Katrolli <ennio_cano@live.com>
2022-10-04 15:56:33 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _abs - main func
+ * Return: returns 0 if sucess
+ * @n: paramater for the function
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		n = (-1) * n;
+	}
+	return (0);
+}
