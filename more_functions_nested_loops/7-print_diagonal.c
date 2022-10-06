d00dd2196e2575f2d78a9053bd847caae6338077
Katrolli <ennio_cano@live.com>
2022-10-06 04:55:33 -0700

    diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * print_line - main func
+ * Return: returns - if success
+ *@n: is a paramater
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		if (i == n - 1)
+			continue;
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
