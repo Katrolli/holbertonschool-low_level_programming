26b677e4a5c2dada99ac5753eabe4428a968c6b1
Katrolli <ennio_cano@live.com>
2022-10-06 02:40:51 -0700

    multi

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * mul - main func
+ * Return: returns - if success
+ * @a: is a parameter
+ * @b: is a paramater
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
