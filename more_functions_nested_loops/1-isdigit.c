ae11b008542948c2467b905cf6f61d44d85d208b
Katrolli <ennio_cano@live.com>
2022-10-06 02:32:17 -0700

    isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _isupper - main func
+ * Return: returns - if success
+ * @c: is a parameter
+ */
+int _isdigit(int c)
+{
+
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
