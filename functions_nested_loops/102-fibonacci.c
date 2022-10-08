a5f0829a1b5ee7bf128cb01118d7ff8fd1517833
Katrolli <ennio_cano@live.com>
2022-10-08 03:04:45 -0700

    fibua

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - this is the entry point of the program
+ * Return: returns 0 if sucess
+ */
+
+
+
+int main(void)
+{
+ 	long int i = 1;
+	long int j = 2;
+
+	printf("%ld, ", i);
+	printf("%ld, ", j);
+		for (int x = 0; x < 48; x++)
+		{
+			long int next = i + j;
+			i = j;
+			j = next;
+			printf("%ld", next);
+			if (x == 47 )
+				continue;
+			printf(", ");
+
+		}
+	printf("\n");
+}
