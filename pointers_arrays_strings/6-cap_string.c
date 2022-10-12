c711b0eb7f2585050e8dbcd3564c4b8af8305fc2
Katrolli <ennio_cano@live.com>
2022-10-12 08:07:42 -0700

    ozot

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,14 +13,14 @@ char *cap_string(char *srt)
 		srt[i] -= 32;
 	for (i = 0; srt[i] != '\0'; i++)
 	{
-		if ((srt[i] > 0 && srt[i] < 65) || (srt[i] > 90 && srt[i] < 97))
+		if ((srt[i] > 0 && srt[i] < 47) || (srt[i] > 90 && srt[i] < 97))
 		{
 			i++;
 			if (srt[i] >= 'a' && srt[i] <= 'z')
 				srt[i] -= 32;
 			i--;
 		}
-		if (srt[i] > 122)
+		if (srt[i] > 122 || (srt[i] > 57 && srt[i] < 65))
 		{
 			i++;
 			if (srt[i] >= 'a' && srt[i] <= 'z')
