7951d9c4cff39b9cdc0a93723cc8eaada9b0923d
Katrolli <ennio_cano@live.com>
2022-10-12 06:07:37 -0700

    toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - main func
+ *
+ * @src: is a parameter
+ * Return: return 1
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
