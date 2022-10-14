ad27cfe0aa330353176770d5aa7d072957e190e4
Katrolli <ennio_cano@live.com>
2022-10-14 03:42:53 -0700

    copy

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - main func
+ *
+ * @s: is a parameter
+ * @c: is a parameter
+ * Return: return
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i])
+		{
+			return (&s[i]);
+		}
+
+	}
+	return (NULL);
+}
