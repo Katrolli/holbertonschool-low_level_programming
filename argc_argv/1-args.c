6b714c003d25809644e27f235217a17af1f4c4ff
Katrolli <ennio_cano@live.com>
2022-10-19 07:23:17 -0700

    arguments nr

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main program
+ * @argc: number of arguments
+ * @argv: pointer to an array of chars
+ * Return: return 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
