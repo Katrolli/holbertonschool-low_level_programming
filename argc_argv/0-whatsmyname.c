1a9e058e320e8fe6952fbdcd14ec660113f22456
Katrolli <ennio_cano@live.com>
2022-10-19 07:02:08 -0700

    name printing

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main program
+ * @argc: number of arguments
+ * @argv: pointer to an array of chars
+ * Return: return 0
+ */
+
+int main(int  __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
