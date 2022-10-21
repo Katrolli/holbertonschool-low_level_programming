97402eb2a2f32920d8d63e5ad2fdf68bada01c16
Katrolli <ennio_cano@live.com>
2022-10-21 03:55:32 -0700

    prime

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -10,31 +10,29 @@
 int main(void)
 {
 	long int num = 612852475143;
-	 long int maxPrime;
-        long int i;
+	long int maxPrime;
+	long int i;
 
-        if (num % 2 == 0)
-        {
-                maxPrime = 2;
-                while ( num % 2 == 0)
-                {
-                        num = num / 2;
-                }
-        }
-        for ( i = 3; i < sqrt(num); i = i + 2)
-        {
-                while (num % i == 0)
-                {
-                        maxPrime = i;
-                        num = num / i;
-                }
-        }
-        if (num > 2)
-        {
-                maxPrime = num;
-        }
+	if (num % 2 == 0)
+	{
+		maxPrime = 2;
+		while (num % 2 == 0)
+		{
+			num = num / 2;
+		}
+	}
+	for (i = 3; i < sqrt(num); i = i + 2)
+	{
+		while (num % i == 0)
+		{
+			maxPrime = i;
+			num = num / i;
+		}
+		if (num > 2)
+		{
+			maxPrime = num;
+		}
 
-
-	printf("Largest prime is : %ld\n", maxPrime);
+	printf("%ld\n", maxPrime);
 	return (0);
 }
