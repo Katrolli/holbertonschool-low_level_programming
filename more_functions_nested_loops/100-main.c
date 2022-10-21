9a12faf5e50c2c786e9eee334334a7a7a34146a6
Katrolli <ennio_cano@live.com>
2022-10-21 03:48:43 -0700

    prime

--- more_functions_nested_loops/100-main.c
+++ more_functions_nested_loops/100-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "main.h"
+#include <math.h>
 
 /**
  * main - main function
@@ -8,8 +9,32 @@
 
 int main(void)
 {
-	long int x = 612852475143;
+	long int num = 612852475143;
+	 long int maxPrime;
+        long int i;
 
-	printf("Largest prime is : %ld\n", Largeprime(x));
+        if (num % 2 == 0)
+        {
+                maxPrime = 2;
+                while ( num % 2 == 0)
+                {
+                        num = num / 2;
+                }
+        }
+        for ( i = 3; i < sqrt(num); i = i + 2)
+        {
+                while (num % i == 0)
+                {
+                        maxPrime = i;
+                        num = num / i;
+                }
+        }
+        if (num > 2)
+        {
+                maxPrime = num;
+        }
+
+
+	printf("Largest prime is : %ld\n", maxPrime);
 	return (0);
 }
--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
-#include <math.h>
 #include "main.h"
+#include <math.h>
 
 /**
- * Largeprime - main function
- * @num: is a paramater
+ * main - main function
  * Return: returns 0 if succesful
  */
 
-long int Largeprime(long int num)
+int main(void)
 {
-	long int maxPrime;
-	long int i;
+	long int num = 612852475143;
+	 long int maxPrime;
+        long int i;
 
-	if (num % 2 == 0)
-	{
-		maxPrime = 2;
-		while ( num % 2 == 0)
-		{
-			num = num / 2;
-		}
-	}
-	for ( i = 3; i < sqrt(num); i = i + 2)
-	{
-		while (num % i == 0)
-		{
-			maxPrime = i;
-			num = num / i;
-		}
-	}
-	if (num > 2)
-	{
-		maxPrime = num;
-	}
-	return maxPrime;
+        if (num % 2 == 0)
+        {
+                maxPrime = 2;
+                while ( num % 2 == 0)
+                {
+                        num = num / 2;
+                }
+        }
+        for ( i = 3; i < sqrt(num); i = i + 2)
+        {
+                while (num % i == 0)
+                {
+                        maxPrime = i;
+                        num = num / i;
+                }
+        }
+        if (num > 2)
+        {
+                maxPrime = num;
+        }
+
+
+	printf("Largest prime is : %ld\n", maxPrime);
+	return (0);
 }
