bc462fbcd154accdb4c4101baf44b313354983af
Katrolli <ennio_cano@live.com>
2022-10-23 11:34:41 -0700

    printing numbers w putchar

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,21 @@
  */
 void print_number(int n)
 {
+	unsigned int m;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		m = -n;
 	}
-	if (n / 10)
+	else
 	{
-		print_number(n / 10);
+		m = n;
 	}
-	_putchar(n % 10 + '0');
+
+	if (m / 10)
+	{
+		print_number(m / 10);
+	}
+	_putchar(m % 10 + '0');
 }
