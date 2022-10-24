bced1d0067f4afcedf894d2bb66b2167dd60d777
Katrolli <ennio_cano@live.com>
2022-10-24 03:37:32 -0700

    malloc and free

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *create_array - main function
+ * @size: is a paramater of the size of the array
+ * @c: paramater that is printed from the function
+ * Return: pointer to an array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i = 0;
+
+	if (size == 0)
+		return (NULL);
+	ptr = malloc(size);
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
+	free(ptr);
+}
