5320ad8780740c7846d32ad3ec15b4c3c0a8ed72
Katrolli <ennio_cano@live.com>
2022-10-25 07:22:55 -0700

    array

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min,int max)
 	ptr = malloc(len * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i <= len; i++)
+	for (i = min; i <= len; i++)
 	{
 		ptr[i] = min;
 		min++;
