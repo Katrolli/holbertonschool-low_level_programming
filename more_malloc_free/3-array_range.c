cca082ad70e5299923ef081c8077c691fd017aad
Katrolli <ennio_cano@live.com>
2022-10-25 07:15:58 -0700

    arra

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - main function
+ * @min: min elemnts of array
+ * @max: max size of elements of array
+ * Return: return the pointer
+ */
+int *array_range(int min,int max)
+{
+	int *ptr;
+	int len = max - min;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	ptr = malloc(len * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i <= len; i++)
+	{
+		ptr[i] = min;
+		min++;
+	}
+	return (ptr);
+}
