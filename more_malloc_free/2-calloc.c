6664fb5c01168758209c4bb6b30f40235ce9ee25
Katrolli <ennio_cano@live.com>
2022-10-25 05:59:08 -0700

    callocl

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - main function
+ * @nmemb: number of elements to add memory
+ * @size: size of elements
+ * Return: return the pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = calloc(nmemb, sizeof(size));
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
