89b5488174edaf163ddc122cf40561b1bdc93167
Katrolli <ennio_cano@live.com>
2022-10-27 04:43:17 -0700

    realloc

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -3,26 +3,27 @@
 #include "main.h"
 
 /**
- * array_range - main function
- * @min: min elemnts of array
- * @max: max size of elements of array
- * Return: return the pointer
+ * _realloc - main function
+ * @old_size: size of old array
+ * @new_size: size of new array
+ * @ptr: is a paramater that  points to old array
+ * Return: returns new pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	char *new_ptr;
-	
+
 	if (new_size == old_size)
-		return ptr;
-	if ( new_size == 0 && ptr != NULL)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
-		return NULL;
+		return (NULL);
 	}
-	if(ptr == NULL)
+	if (ptr == NULL)
 		new_ptr = malloc(new_size);
 	else
 		new_ptr = malloc(old_size + new_size - 30);
 	free(ptr);
-	return new_ptr;
+	return (new_ptr);
 }
