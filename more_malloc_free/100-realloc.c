28603d998bd426e0c5149a6440fef97dd2f90be5
Katrolli <ennio_cano@live.com>
2022-10-27 04:32:20 -0700

    realloc

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -14,6 +14,11 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	
 	if (new_size == old_size)
 		return ptr;
+	if ( new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return NULL;
+	}
 	if(ptr == NULL)
 		new_ptr = malloc(new_size);
 	else
