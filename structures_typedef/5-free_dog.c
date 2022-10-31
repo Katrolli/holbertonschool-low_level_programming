85e41aff404e8eb40ec4699c522afd1c50edfa45
Katrolli <ennio_cano@live.com>
2022-10-31 07:42:13 -0700

    free memory

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Defining a dog struct
+ * @d: old pointer
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name;
+		free(d->owner);
+		free(d);
+	}
+}
