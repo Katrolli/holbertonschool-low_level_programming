45c0028e02afebad271b74fa6a05f928cb70da8f
Katrolli <ennio_cano@live.com>
2022-10-31 04:16:09 -0700

    Structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * struct dog - Defining a dog struct
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
