ab3b6b592461ca2dbcab46c3e44340ddc35503e5
Katrolli <ennio_cano@live.com>
2022-10-31 05:18:28 -0700

    newdoggy

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - Defining a dog struct
+ * @name: First member
+ * @age: second member
+ * owner: third member
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_d;
+	new_d = malloc(sizeof(dog_t));
+	if (new_d == NULL)
+		return (NULL);
+	new_d->name = name;
+	new_d->age = age;
+	new_d->owner = owner;
+	return (new_d);
+}
