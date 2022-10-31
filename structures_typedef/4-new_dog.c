dab09fe74d1dd3d3730c52cad30a28c747695df4
Katrolli <ennio_cano@live.com>
2022-10-31 05:24:12 -0700

    oof

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,8 +11,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_d;
 	new_d = malloc(sizeof(dog_t));
+	new_d->name = malloc(sizeof(char));
+	new_d->owner = malloc(sizeof(char));
+
 	if (new_d == NULL)
+	{
+		free(new_d);
 		return (NULL);
+	}
 	new_d->name = name;
 	new_d->age = age;
 	new_d->owner = owner;
