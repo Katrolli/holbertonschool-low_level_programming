75080f4c695ae4d40cf45b76e62e500bc5e8fd34
Katrolli <ennio_cano@live.com>
2022-10-31 05:31:26 -0700

    newdog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,8 +11,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_d;
 	new_d = malloc(sizeof(dog_t));
-	new_d->name = malloc(sizeof(char));
-	new_d->owner = malloc(sizeof(char));
 
 	if (new_d == NULL)
 	{
