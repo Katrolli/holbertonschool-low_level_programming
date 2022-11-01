4ac5cbfff9274955eebb454a6a26e1520b4bd0c6
Katrolli <ennio_cano@live.com>
2022-11-01 04:47:19 -0700

    Printing with function to pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - main function
+ * @name: first paramater
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char*))
+{
+	if (f)
+		(*f)(name);
+}
