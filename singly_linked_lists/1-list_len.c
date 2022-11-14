e5590fea9884811fb0c1ff7d1cd120b6d7ecf153
Katrolli <ennio_cano@live.com>
2022-11-14 07:23:47 -0800

    Printing nr of elements

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - main func
+ * @h: pointer to list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
