686ace55c62a36dfc7f5fb157c0d571b3cc7f725
Katrolli <ennio_cano@live.com>
2022-11-15 08:33:09 -0800

    adding at index

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (count != idx - 1)
 	{
 		if (h == NULL)
+		{
+			free(tmp);
 			return (NULL);
+		}
 		h = h->next;
 		count++;
 	}
