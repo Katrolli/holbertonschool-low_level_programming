9c605221eacb96a45801aa1ec3810ca82b80e703
Katrolli <ennio_cano@live.com>
2022-11-15 12:18:36 -0800

    test

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -13,6 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
         {
+		*head = malloc(sizeof(listint_t));
                 (*head)->n = n;
                 (*head)->next = NULL;
                 return (*head);
