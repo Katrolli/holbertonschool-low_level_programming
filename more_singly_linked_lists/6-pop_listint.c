cf67b5c1dc0bf6369f0fcb3c2bb27dfb17feceb1
Katrolli <ennio_cano@live.com>
2022-11-15 07:16:43 -0800

    removing old head

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -7,10 +7,11 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *ptr;
-	int n = (*head)->n;
+	int n;
 
 	if (*head == NULL)
 		return (0);
+	n = (*head)->n;
 	ptr = *head;
 	*head = (*head)->next;
 	free(ptr);
