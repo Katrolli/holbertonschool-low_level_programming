b6642bcf6ae087e8ff503ee2090fa8e380a54df8
Katrolli <ennio_cano@live.com>
2022-11-15 07:32:17 -0800

    node at index

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (count != index)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 		count++;
 	}
 	return (head);
