df81479163563b7e43853340021667aecd7fa942
Katrolli <ennio_cano@live.com>
2022-11-15 07:13:48 -0800

    removing old head

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - main func
+ * @head: pointer to list
+ * Return: return the value of the old head node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr;
+	int n = (*head)->n;
+
+	if (head == NULL)
+		return (0);
+	ptr = *head;
+	*head = (*head)->next;
+	free(ptr);
+	ptr = NULL;
+	return (n);
+}
