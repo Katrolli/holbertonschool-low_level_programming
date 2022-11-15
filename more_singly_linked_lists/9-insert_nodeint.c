f48b4dce7df7db2d8780a79e6262d60760f10b52
Katrolli <ennio_cano@live.com>
2022-11-15 12:23:10 -0800

    test

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (h->next == NULL)
 		{
 			h->next = tmp;
+			*head = tmp;
 			return (tmp);
 		}
 		tmp->next = h->next;
