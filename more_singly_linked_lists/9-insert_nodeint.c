fdeee2c7844f54e2d6876023073ecc44bf4c309e
Katrolli <ennio_cano@live.com>
2022-11-15 08:29:05 -0800

    adding at index

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -14,7 +14,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	tmp = malloc(sizeof(listint_t));
 	tmp->n = n;
 	tmp->next = NULL;
+	if (head == NULL)
+		return (NULL);
 	h = *head;
+	if (idx == 0)
+	{
+		tmp->next = *head;
+		*head = tmp;
+	}
 	while (count != idx - 1)
 	{
 		if (h == NULL)
