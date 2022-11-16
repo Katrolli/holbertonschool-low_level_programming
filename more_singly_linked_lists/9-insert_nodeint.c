51f9778ee45d8fab4e4f3ba527a426cc457273a9
Katrolli <ennio_cano@live.com>
2022-11-16 04:06:03 -0800

    testing

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		tmp->next = h;
 		*head = tmp;
-		return (*head);
+		return(tmp);
 	}
 	while (count != idx - 1)
 	{
