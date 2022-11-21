5aaf3621ff96ee3c4fdeed4af21aef84f6587f6b
Katrolli <ennio_cano@live.com>
2022-11-21 06:36:07 -0800

    append

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	openf = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
 	if (openf == -1)
 		return (-1);
-	if(text_content == NULL)
+	if (text_content == NULL)
 		return (1);
 	writef = write(openf, text_content, strlen(text_content));
 	if (writef == -1)
