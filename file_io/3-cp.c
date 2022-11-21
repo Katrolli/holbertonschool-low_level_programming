e9370ef4742a81bb336e5ec3f139c84d93ef0fac
Katrolli <ennio_cano@live.com>
2022-11-21 15:44:23 -0800

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -31,11 +31,6 @@ int main (int argc, char *argv[])
                 dprintf(2, "Error: Can't write to %s\n", argv[2]);
                 exit(99);
         }
-	if ((file_read = read(file_from,buff, file_from_size) == -1))
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		 exit(98);
-	}
 	while ((file_read = read(file_from,buff,file_from_size)) > 0)
 	{
 		if (file_read == -1)
@@ -50,6 +45,12 @@ int main (int argc, char *argv[])
 			 exit(99);
 		}
 	}
+	 if ((file_read = read(file_from,buff, file_from_size) == -1))
+        {
+                dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+                 exit(98);
+        }
+
 	cl1 = close(file_from);
 	cl2 = close(file_to);
 	if (cl1 == -1)
