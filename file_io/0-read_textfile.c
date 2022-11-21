1871405f3362428683d8fde06922bef8c6d51be7
Katrolli <ennio_cano@live.com>
2022-11-21 05:18:22 -0800

    File io

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * read_textfile - main function
+ * @filename: source of text
+ * @letters: numbers of characters
+ * Return: returns the number of chars printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int openf, readf, writef;
+	char *txt;
+
+	if (filename == NULL)
+		return (0);
+	openf = open(filename, O_RDONLY);
+	if (openf == -1)
+		return (0);
+	txt = malloc(sizeof(char) * letters);
+	readf = read(openf, txt, letters);
+	if (readf == -1)
+		return (0);
+	writef = write(1, txt, letters);
+	if (writef == -1)
+		return (0);
+	close(openf);
+	return (readf);
+}
