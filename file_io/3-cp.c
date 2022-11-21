be742c2cb80b3df93d3d378645724e4c74782b0e
Katrolli <ennio_cano@live.com>
2022-11-21 15:16:12 -0800

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -23,7 +23,7 @@ int main (int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
         {
-                dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+                dprintf(2, "Error: Can't write to %s\n", argv[2]);
                 exit(99);
         }
 	while ((file_read = read(file_from,buff,file_from_size)) > 0)
@@ -36,7 +36,7 @@ int main (int argc, char *argv[])
 		file_write = write(file_to, buff, file_from_size);
 		if (file_write == -1)
 		{
-			 dprintf(2, "Error: Can't write to file %s\n", argv[2]);
+			 dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			 exit(99);
 		}
 	}
