f8ad08949e7988c69749365da4f5f3d6be0237d1
Katrolli <ennio_cano@live.com>
2022-11-22 05:15:24 -0800

    printing elements of list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,16 @@
 #include "lists.h"
+/**
+ * print_dlistint - function to print elements of list
+ * @h: header of list
+ * Return: the count after iteration
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0;
 
 	if (h == NULL)
 		return (i);
-	while(h != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		i++;
