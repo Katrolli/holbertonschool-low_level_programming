821b3e8dcd47e4a1d3c45b1ae42fdc3c7e7b29f1
Katrolli <ennio_cano@live.com>
2022-11-22 05:13:09 -0800

    printing elements of list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (i);
+	while(h != NULL)
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
