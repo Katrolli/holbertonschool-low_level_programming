0c7d7aa944af4587738da06e4c305d002f477e64
Katrolli <ennio_cano@live.com>
2022-11-22 09:54:13 -0800

    deleting

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - function to count elements of list
+ * @h: header of list
+ * @idx: index to add node
+ * Return: the new added node
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *tmp, *del;
+	unsigned int count = 0;
+
+	if (*head == NULL)
+		return (-1);
+	tmp = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		tmp->next = NULL;
+		tmp->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+	else
+	{
+		for (count = 0; count < index - 1; count++)
+		{
+			if (tmp == NULL)
+				return (-1);
+			tmp = tmp->next;
+		}
+		del = tmp->next;
+		tmp->next = tmp->next->next;
+		del->next = NULL;
+		free(del);
+		return(1);
+	}
+	return (-1);
+}
