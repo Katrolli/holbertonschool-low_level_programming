6a11a287517ba989f24dcce262e70de983153efc
Katrolli <ennio_cano@live.com>
2022-11-22 05:37:25 -0800

    add new node at the start

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_dnodeint - function to count elements of list
+ * @head: header of list
+ * @n: data to add
+ * Return: the count after iteration
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	*head = new;
+	return (*head);
+}
