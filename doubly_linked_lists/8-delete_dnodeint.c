979d72c0ffb622bb159282b2c1099813a8c5a1ac
Katrolli <ennio_cano@live.com>
2022-11-22 10:26:50 -0800

    testing

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	tmp = *head;
 	if (index == 0)
 	{
-		*head = (*head)->next->next;
+		*head = (*head)->next->prev;
 		tmp->next = NULL;
 		tmp->prev = NULL;
 		free(tmp);
