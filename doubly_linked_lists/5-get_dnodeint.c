27d732e1f24ef31fc902b622d4addfa697c9361a
Katrolli <ennio_cano@live.com>
2022-11-22 07:26:13 -0800

    get index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_dlistint - function to count elements of list
+ * @head: header of list
+ * @index: position of node to display
+ * Return: the node of index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	while (head == NULL)
+		return (NULL);
+	count = 0;
+	while (head != NULL)
+	{
+		if (count == index)
+			return (head);
+		head = head->next;
+		count++;
+	}
+	return (head);
+}
