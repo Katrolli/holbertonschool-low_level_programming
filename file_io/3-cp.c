36c676101e67f8c150c1b136ceeab91308e9dbd7
Katrolli <ennio_cano@live.com>
2022-11-21 16:04:11 -0800

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "main.h"
-int main (int argc, char *argv[])
+/**
+ * main - main func
+ * @argc: par
+ * @argv: par
+ * Return: 0
+ */
+int main(int argc, char *argv[])
 {
-	int file_from, cl1, cl2, file_to,file_read,file_write;
+	int file_from, cl1, cl2, file_to, file_read, file_write;
 	char *buff[1024];
 	int tmp_file_from = open(argv[1], O_RDONLY);
-	size_t file_from_size = (size_t)lseek(tmp_file_from,0,SEEK_END);
-	close(tmp_file_from);
+	size_t file_from_size = (size_t)lseek(tmp_file_from, 0, SEEK_END);
 
+	close(tmp_file_from);
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
@@ -22,36 +28,35 @@ int main (int argc, char *argv[])
 	}
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
-        {
-                dprintf(2, "Error: Can't write to %s\n", argv[2]);
-                exit(99);
-        }
-	while ((file_read = read(file_from,buff,file_from_size)) > 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((file_read = read(file_from, buff, file_from_size)) > 0)
 	{
 		file_write = write(file_to, buff, file_from_size);
 		if (file_write == -1)
 		{
-			 dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			 exit(99);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
-	 if ((file_read = read(file_from,buff, file_from_size) == -1))
-        {
-                dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-                 exit(98);
-        }
-
+	if ((file_read = read(file_from, buff, file_from_size) == -1))
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	cl1 = close(file_from);
 	cl2 = close(file_to);
 	if (cl1 == -1)
 	{
 		dprintf(2, "Error: Can't close fd %i\n", cl1);
-		exit (100);
+		exit(100);
 	}
 	if (cl2 == -1)
-        {
-	dprintf(2, "Error: Can't close fd %i\n", cl2);
-		exit (100);
+	{
+		dprintf(2, "Error: Can't close fd %i\n", cl2);
+		exit(100);
 	}
 	return (0);
 }
