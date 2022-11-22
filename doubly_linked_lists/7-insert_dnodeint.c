a87fd168fb8f945da652e7a05a8e0baa960f604c
Katrolli <ennio_cano@live.com>
2022-11-22 08:50:44 -0800

    testing

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,6 +21,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp = *h;
 	if (idx == 0)
 	{
+		if (*h == NULL)
+		{
+			*h = n;
+			return (*h);
+		}
 		new->next = *h;
 		(*h)->prev = new;
 		*h = new;
@@ -43,5 +48,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	tmp->next->prev = new;
 	tmp->next = new;
 	new->prev = tmp;
-	return (tmp);
+	return (new);
 }
