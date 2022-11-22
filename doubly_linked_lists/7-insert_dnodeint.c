c79d66a68c65b2dcfc1d4f9b6c62355afb083eb2
Katrolli <ennio_cano@live.com>
2022-11-22 08:39:34 -0800

    adding at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -34,8 +34,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	if (tmp->next == NULL)
 	{
-		new->prev = tmp;
 		tmp->next = new;
+		new->prev = tmp;
 		return (new);
 	}
 	new->next = tmp->next;
