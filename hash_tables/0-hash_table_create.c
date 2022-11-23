0fbc2f8727893cdb947ac9888d647b11990e5685
Katrolli <ennio_cano@live.com>
2022-11-23 06:48:30 -0800

    testing

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	arr->size = size;
-	arr->array = malloc(sizeof(hash_node_t) * sizeof(hash_table_t));
+	arr->array = malloc(sizeof(hash_node_t) * sizeof(char *));
 		if (arr->array == NULL)
 			return (NULL);
 	return (arr);
