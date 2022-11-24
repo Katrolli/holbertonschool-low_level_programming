0a7de2890f08f3d21f4f5d4998e6c800425a59d8
Katrolli <ennio_cano@live.com>
2022-11-24 05:32:46 -0800

    testing adding node

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -8,17 +8,28 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	hash_node_t *new;
+	hash_node_t *new, *current;
 
 	if (key == NULL || ht == NULL)
 		return (0);
-	 idx = key_index((const unsigned char *)key, ht->size);
-    new = malloc(sizeof(hash_node_t));
-    if (new == NULL)
-        return 0;
-    new->key = strdup(key);
-    new->value = strdup(value);
-    new->next = ht->array[idx];
-    ht->array[idx] = new;
-    return (1);
+	idx = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[idx];
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return 1;
+		}
+		current = current->next;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return 0;
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
 }
